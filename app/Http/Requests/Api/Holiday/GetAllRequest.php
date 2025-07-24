3e1c2d48006d8fb099b526fdbb4442e640d2d6f0
Tom Patrick Hanks <58249589+TomPatricHanks@users.noreply.github.com>
2025-07-24 09:38:22 +0300

    Fixed Depolyment Bugs

--- app/Http/Requests/Api/Holiday/GetAllRequest.php
+++ app/Http/Requests/Api/Holiday/GetAllRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Api\Holiday;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class GetHolidayRequest extends FormRequest
+class GetAllRequest extends FormRequest
 {
   /**
    * Determine if the user is authorized to make this request.
